3b6b44ee239f9ceddc6e071b9386a52581fe4834
Jason Hou <Jason@localhost.localdomain>
2015-11-04 12:08:30 -0500

    mm.c template

---
+++ mm.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <assert.h>
+
+#include "mm.h"
+#include "memlib.h"
+
+team_t team = {
+    /* Team name */
+    "",
+    /* First member's full name */
+    "Jue Hou",
+    "hjue@vt.edu",
+    /* Second member's full name (leave blank if none) */
+    "Andrew K Knittle",
+    "andrk11@vt.edu",
+}; 
